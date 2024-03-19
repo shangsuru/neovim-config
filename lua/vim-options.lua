d4c24c2f545045e1770265bcfd1c3ebd10092edd
shangsuru <45686061+shangsuru@users.noreply.github.com>
2024-03-19 22:21:37 +0900

    Add keymaps for tabs and splits

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,4 +5,20 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 
-
+-- New tab
+vim.keymap.set("n", "te", ":tabedit")
+vim.keymap.set("n", "<tab>", ":tabnextReturn>", {})
+vim.keymap.set("n", "<s-tab>", ":tabprev<Return>", {})
+-- Split window
+vim.keymap.set("n", "ss", ":split<Return>", {})
+vim.keymap.set("n", "sv", ":vsplit<Return>", {})
+-- Move window
+vim.keymap.set("n", "sh", "<C-w>h")
+vim.keymap.set("n", "sk", "<C-w>k")
+vim.keymap.set("n", "sj", "<C-w>j")
+vim.keymap.set("n", "sl", "<C-w>l")
+-- Resize window
+vim.keymap.set("n", "<C-w><left>", "<C-w><")
+vim.keymap.set("n", "<C-w><right>", "<C-w>>")
+vim.keymap.set("n", "<C-w><up>", "<C-w>+")
+vim.keymap.set("n", "<C-w><down>", "<C-w>-")
