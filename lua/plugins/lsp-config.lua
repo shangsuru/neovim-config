0ead73a9867631afd913d608ec1834deb2cd7ee3
shangsuru <45686061+shangsuru@users.noreply.github.com>
2024-04-04 09:31:06 +0900

    Add go config and autoformat on save

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,11 +32,14 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
+      lspconfig.gopls.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,9 +7,27 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.diagnostics.erb_lint,
-                                null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.formatting.golines,
 			},
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({
+            group = augroup,
+            buffer = bufnr,
+        })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ async = false })
+          end,
+        })
+        end
+      end,
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
